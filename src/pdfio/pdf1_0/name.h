1e36eb9e6e0fd772e6fad619b74660b01860999b
Vladyslav Kazachenko <vladyslav.kazachenko@gmail.com>
2018-02-16 22:57:14 +0100

    pdf1_0::name.h: update doc

--- src/pdfio/pdf1_0/name.h
+++ src/pdfio/pdf1_0/name.h
@@ -7,24 +7,24 @@ namespace pdfio
 	
 namespace pdf1_0
 {
-	/*! \brief PDF name object.
-	 */
+	/*! \brief PDF name object.*/
 	class Name
 	{
 	public:
+		/*! \brief Default constructor*/
 		Name() = default;
-		
+		/*! \brief Initializes name by std::string value*/
 		inline Name(const std::string &value)
 		: value_(value)
 		{
 			
 		}
-		
+		/*! \brief Compares name with std::string value*/
 		inline bool operator==(const std::string &value)
 		{
 			return value_ == value;
 		}
-		
+		/*! \brief Compares name with Name value*/
 		inline bool operator==(const Name &other)
 		{
 			return value_ == other.value_;

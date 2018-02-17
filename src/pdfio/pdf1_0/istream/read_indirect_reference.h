04dfbe26a013366e08dd2f6be0c070da59c0dc05
Vladyslav Kazachenko <vladyslav.kazachenko@gmail.com>
2018-02-17 22:47:44 +0100

    pdf1_0::IndirectReference: add operator>>

---
+++ src/pdfio/pdf1_0/istream/read_indirect_reference.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <istream>
+
+#include "pdfio/pdf1_0/indirect_reference.h"
+
+namespace pdf1_0 = pdfio::pdf1_0;
+/*! \brief Reads the indirectReference from the istream*/
+std::istream &operator>>(std::istream &istream, pdf1_0::IndirectReference &indirectReference)
+{
+	auto objNumBuffer = indirectReference.objectNumber();
+	if(istream >> objNumBuffer)
+	{
+		auto genNumBuffer = indirectReference.generationNumber();
+		if(istream >> genNumBuffer)
+		{
+			char ch = 0;
+			if(istream >> ch)
+			{
+				if(ch == 'R')
+				{
+					indirectReference.objectNumber() = objNumBuffer;
+					indirectReference.generationNumber() = genNumBuffer;
+				}
+				else
+				{
+					istream.setstate(std::ios_base::failbit);
+				}
+			}
+		}
+	}
+	return istream;
+}
---
+++ test/pdfio/pdf1_0/istream/pdf1_0_istream_indirect_reference_testsuite.cpp
@@ -0,0 +1,57 @@
+#include <sstream>
+
+#include "pdfio/pdf1_0/istream/read_indirect_reference.h"
+#include "gtest/gtest.h"
+
+namespace pdf1_0 = pdfio::pdf1_0;
+
+TEST(IndirectReferenceTestSuite, emptyStream)
+{
+	pdf1_0::IndirectReference indirectReference;
+	std::istringstream istream;
+	EXPECT_FALSE(istream >> indirectReference);
+}
+
+TEST(IndirectReferenceTestSuite, wrongObjectNumber)
+{
+	pdf1_0::IndirectReference indirectReference;
+	std::istringstream istream("whatever");
+	EXPECT_FALSE(istream >> indirectReference);
+}
+
+TEST(IndirectReferenceTestSuite, wrongGenerationNumber)
+{
+	pdf1_0::IndirectReference indirectReference1;
+	std::istringstream istream1("2");
+	EXPECT_FALSE(istream1 >> indirectReference1);
+	
+	pdf1_0::IndirectReference indirectReference2;
+	std::istringstream istream2("2 whatever R");
+	EXPECT_FALSE(istream2 >> indirectReference2);
+}
+
+TEST(IndirectReferenceTestSuite, wrongReference)
+{
+	pdf1_0::IndirectReference indirectReference1;
+	std::istringstream istream1("2 1");
+	EXPECT_FALSE(istream1 >> indirectReference1);
+	
+	pdf1_0::IndirectReference indirectReference2;
+	std::istringstream istream2("2 1 r");
+	EXPECT_FALSE(istream2 >> indirectReference2);
+}
+
+TEST(IndirectReferenceTestSuite, ok)
+{
+	pdf1_0::IndirectReference indirectReference1;
+	std::istringstream istream1("2 1 R");
+	EXPECT_TRUE(istream1 >> indirectReference1);
+	EXPECT_EQ(2, indirectReference1.objectNumber());
+	EXPECT_EQ(1, indirectReference1.generationNumber());
+	
+	pdf1_0::IndirectReference indirectReference2;
+	std::istringstream istream2("45 2 R");
+	EXPECT_TRUE(istream2 >> indirectReference2);
+	EXPECT_EQ(45, indirectReference2.objectNumber());
+	EXPECT_EQ(2, indirectReference2.generationNumber());
+}

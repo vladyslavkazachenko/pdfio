458d185ad23f45fee89ca643a3da295122a5daad
Vladyslav Kazachenko <vladyslav.kazachenko@gmail.com>
2018-02-18 15:06:03 +0100

    pdf1_0::FileHeader: remove all methods and data

--- src/pdfio/pdf1_0/file_header.h
+++ src/pdfio/pdf1_0/file_header.h
@@ -8,31 +8,7 @@ namespace pdf1_0
 	/*! \brief PDF file header.*/
 	class FileHeader
 	{
-	public:
-		/*! \brief Returns PDF version's major component.*/
-		inline const int &majorVersion() const
-		{
-			return majorVersion_;
-		}
-		/*! \brief Returns PDF version's major component.*/
-		inline int &majorVersion()
-		{
-			return majorVersion_;
-		}
-		/*! \brief Returns PDF version's minor component.*/
-		inline const int &minorVersion() const
-		{
-			return minorVersion_;
-		}
-		/*! \brief Returns PDF version's minor component.*/
-		inline int &minorVersion()
-		{
-			return minorVersion_;
-		}
-		
-	private:
-		int majorVersion_ = 1;
-		int minorVersion_ = 0;
+
 	};
 }
 
--- test/pdfio/pdf1_0/pdf1_0_file_header_testsuite.cpp
+++ test/pdfio/pdf1_0/pdf1_0_file_header_testsuite.cpp
@@ -6,15 +6,4 @@ namespace pdf1_0 = pdfio::pdf1_0;
 TEST(FileHeaderTestSuite, defaultCtor)
 {
 	pdf1_0::FileHeader fileHeader;
-	EXPECT_EQ(1, fileHeader.majorVersion());
-	EXPECT_EQ(0, fileHeader.minorVersion());
-}
-
-TEST(FileHeaderTestSuite, assignment)
-{
-	pdf1_0::FileHeader fileHeader;
-	fileHeader.majorVersion() = 1;
-	fileHeader.minorVersion() = 3;
-	EXPECT_EQ(1, fileHeader.majorVersion());
-	EXPECT_EQ(3, fileHeader.minorVersion());
 }

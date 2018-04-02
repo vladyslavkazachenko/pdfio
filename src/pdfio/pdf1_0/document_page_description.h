4afcf8ab5935cdf23baec01c5347a7619b550b7e
Vladyslav Kazachenko <vladyslav.kazachenko@gmail.com>
2018-04-02 17:25:51 +0200

    PageDescription: add drawing functions

--- src/pdfio/pdf1_0/document_page_description.h
+++ src/pdfio/pdf1_0/document_page_description.h
@@ -2,6 +2,7 @@
 
 #include <functional>
 #include <cassert>
+#include <vector>
 
 #include "integer.h"
 
@@ -18,6 +19,15 @@ struct DocumentPageDescription
 	std::function<void(Integer x, Integer y)> linetoHandler_;
 	std::function<void()> strokeHandler_;
 	std::function<void(Integer w)> setlinewidthHandler_;
+	std::function<void(Integer x, Integer y, Integer w, Integer h)> rectangleHandler_;
+	std::function<void()> fillHandler_;
+	std::function<void()> fillAndStrokeHandler_;
+	std::function<void()> closepathFillAndStrokeHandler_;
+	std::function<void(Integer x1, Integer y1, Integer x2, Integer y2, Integer x3, Integer y3)> curveHandler_;
+	std::function<void(double r, double g, double b)> setrgbcolorFillHandler_;
+	std::function<void(double r, double g, double b)> setrgbcolorStrokeHandler_;
+	std::function<void(Integer phase, const std::vector<Integer> &array)> setdashHandler_;
+	std::function<void(double gray)> setgrayFillHandler_;
 	
 	inline void moveto(Integer x, Integer y)
 	{
@@ -38,6 +48,51 @@ struct DocumentPageDescription
 	{
 		assert(setlinewidthHandler_);
 		setlinewidthHandler_(w);
+	}
+	inline void rectangle(Integer x, Integer y, Integer w, Integer h)
+	{
+		assert(rectangleHandler_);
+		rectangleHandler_(x, y, w, h);
+	}
+	inline void fill()
+	{
+		assert(fillHandler_);
+		fillHandler_();
+	}
+	inline void fillAndStroke()
+	{
+		assert(fillAndStrokeHandler_);
+		fillAndStrokeHandler_();
+	}
+	inline void closepathFillAndStroke()
+	{
+		assert(closepathFillAndStrokeHandler_);
+		closepathFillAndStrokeHandler_();
+	}
+	inline void curve(Integer x1, Integer y1, Integer x2, Integer y2, Integer x3, Integer y3)
+	{
+		assert(curveHandler_);
+		curveHandler_(x1, y1, x2, y2, x3, y3);
+	}
+	inline void setrgbcolorFill(double r, double g, double b)
+	{
+		assert(setrgbcolorFillHandler_);
+		setrgbcolorFillHandler_(r, g, b);
+	}
+	inline void setrgbcolorStroke(double r, double g, double b)
+	{
+		assert(setrgbcolorStrokeHandler_);
+		setrgbcolorStrokeHandler_(r, g, b);
+	}
+	inline void setdash(Integer phase, const std::vector<Integer> &array)
+	{
+		assert(setdashHandler_);
+		setdashHandler_(phase, array);
+	}
+	inline void setgrayFill(double gray)
+	{
+		assert(setgrayFillHandler_);
+		setgrayFillHandler_(gray);
 	}
 };
 
--- src/pdfio/pdf1_0/istream/read_document_page_description.cpp
+++ src/pdfio/pdf1_0/istream/read_document_page_description.cpp
@@ -15,6 +15,15 @@ namespace
 		kLine,
 		kStroke,
 		kWidth,
+		kRectangle,
+		kFill,
+		kFillAndStroke,
+		kCloseFillAndStroke,
+		kCurve,
+		kRgbFill,
+		kRgbStroke,
+		kDash,
+		kGrayFill,
 		kUnknown,
 	};
 	
@@ -36,6 +45,38 @@ namespace
 		{
 			return Cmd::kWidth;
 		}
+		if(str == "re")
+		{
+			return Cmd::kRectangle;
+		}
+		if(str == "B")
+		{
+			return Cmd::kFillAndStroke;
+		}
+		if(str == "b")
+		{
+			return Cmd::kCloseFillAndStroke;
+		}
+		if(str == "c")
+		{
+			return Cmd::kCurve;
+		}
+		if(str == "rg")
+		{
+			return Cmd::kRgbFill;
+		}
+		if(str == "RG")
+		{
+			return Cmd::kRgbStroke;
+		}
+		if(str == "d")
+		{
+			return Cmd::kDash;
+		}
+		if(str == "g")
+		{
+			return Cmd::kGrayFill;
+		}
 		return Cmd::kUnknown;
 	}
 }
@@ -44,62 +85,253 @@ std::istream &operator>>(std::istream &istream, pdf1_0::DocumentPageDescription 
 {
 	std::vector<std::string> tokens;
 	std::string token;
-	while(!istream.eof() && istream >> token)
+	while(!istream.eof())
 	{
-		switch(String2Cmd(token))
+		std::skipws(istream);
+		char ch;
+		istream >> std::skipws >> ch;
+		istream.unget();
+		if(ch == '%')
 		{
-		case Cmd::kMove:
-			if(tokens.size() >= 2)
+			tokens.clear();
+			char buffer[256];
+			istream.getline(buffer, 256, '\n');
+			continue;
+		}
+		if(ch == '[')
+		{
+			tokens.clear();
+			static_cast<void>(istream.get());
+			char cc;
+			std::string s1;
+			while((istream >> cc) && cc != ']')
 			{
-				std::stringstream ssx(*(tokens.rbegin()));
-				pdf1_0::Integer y;
-				if(ssx >> y)
+				s1 += cc;
+				s1 += " ";
+			}
+			std::stringstream ss(s1);
+			while(ss)
+			{
+				std::string s;
+				if(ss >> s)
 				{
-					std::stringstream ssy(*(tokens.rbegin() + 1));
-					pdf1_0::Integer x;
-					if(ssy >> x)
+					tokens.push_back(s);
+				}
+			}
+			continue;
+		}
+		if(istream >> token)
+		{
+			switch(String2Cmd(token))
+			{
+			case Cmd::kMove:
+				if(tokens.size() >= 2)
+				{
+					std::stringstream ssx(*(tokens.rbegin()));
+					pdf1_0::Integer y;
+					if(ssx >> y)
 					{
-						description.moveto(x, y);
+						std::stringstream ssy(*(tokens.rbegin() + 1));
+						pdf1_0::Integer x;
+						if(ssy >> x)
+						{
+							description.moveto(x, y);
+						}
 					}
 				}
-			}
-			tokens.clear();
-			break;
-		case Cmd::kLine:
-			if(tokens.size() >= 2)
-			{
-				std::stringstream ssx(*(tokens.rbegin()));
-				pdf1_0::Integer y;
-				if(ssx >> y)
+				tokens.clear();
+				break;
+			case Cmd::kLine:
+				if(tokens.size() >= 2)
 				{
-					std::stringstream ssy(*(tokens.rbegin() + 1));
-					pdf1_0::Integer x;
-					if(ssy >> x)
+					std::stringstream ssx(*(tokens.rbegin()));
+					pdf1_0::Integer y;
+					if(ssx >> y)
 					{
-						description.lineto(x, y);
+						std::stringstream ssy(*(tokens.rbegin() + 1));
+						pdf1_0::Integer x;
+						if(ssy >> x)
+						{
+							description.lineto(x, y);
+						}
 					}
 				}
-			}
-			tokens.clear();
-			break;
-		case Cmd::kStroke:
-			description.stroke();
-			tokens.clear();
-			break;
-		case Cmd::kWidth:
-			if(tokens.size() >= 1)
-			{
-				std::stringstream ss(*(tokens.rbegin()));
-				pdf1_0::Integer w;
-				if(ss >> w)
+				tokens.clear();
+				break;
+			case Cmd::kStroke:
+				description.stroke();
+				tokens.clear();
+				break;
+			case Cmd::kWidth:
+				if(tokens.size() >= 1)
 				{
-					description.setlinewidth(w);
+					std::stringstream ss(*(tokens.rbegin()));
+					pdf1_0::Integer w;
+					if(ss >> w)
+					{
+						description.setlinewidth(w);
+					}
 				}
+				tokens.clear();
+				break;
+			case Cmd::kRectangle:
+				if(tokens.size() >= 4)
+				{
+					std::stringstream ssh(*(tokens.rbegin()));
+					pdf1_0::Integer h;
+					if(ssh >> h)
+					{
+						std::stringstream ssw(*(tokens.rbegin() + 1));
+						pdf1_0::Integer w;
+						if(ssw >> w)
+						{
+							std::stringstream ssy(*(tokens.rbegin() + 2));
+							pdf1_0::Integer y;
+							if(ssy >> y)
+							{
+								std::stringstream ssx(*(tokens.rbegin() + 3));
+								pdf1_0::Integer x;
+								if(ssx >> x)
+								{
+									description.rectangle(x, y, w, h);
+								}
+							}
+						}
+					}
+				}
+				tokens.clear();
+				break;
+			case Cmd::kFill:
+				description.fill();
+				tokens.clear();
+				break;
+			case Cmd::kFillAndStroke:
+				description.fillAndStroke();
+				tokens.clear();
+				break;
+			case Cmd::kCloseFillAndStroke:
+				description.closepathFillAndStroke();
+				tokens.clear();
+				break;
+			case Cmd::kCurve:
+				if(tokens.size() >= 6)
+				{
+					std::stringstream ssy3(*(tokens.rbegin()));
+					pdf1_0::Integer y3;
+					if(ssy3 >> y3)
+					{
+						std::stringstream ssx3(*(tokens.rbegin() + 1));
+						pdf1_0::Integer x3;
+						if(ssx3 >> x3)
+						{
+							std::stringstream ssy2(*(tokens.rbegin() + 2));
+							pdf1_0::Integer y2;
+							if(ssy2 >> y2)
+							{
+								std::stringstream ssx2(*(tokens.rbegin() + 3));
+								pdf1_0::Integer x2;
+								if(ssx2 >> x2)
+								{
+									std::stringstream ssy1(*(tokens.rbegin() + 4));
+									pdf1_0::Integer y1;
+									if(ssy1 >> y1)
+									{
+										std::stringstream ssx1(*(tokens.rbegin() + 5));
+										pdf1_0::Integer x1;
+										if(ssx1 >> x1)
+										{
+											description.curve(x1, y1, x2, y2, x3, y3);
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+				tokens.clear();
+				break;
+			case Cmd::kRgbFill:
+				if(tokens.size() >= 3)
+				{
+					std::stringstream ssb(*(tokens.rbegin()));
+					double b;
+					if(ssb >> b)
+					{
+						std::stringstream ssg(*(tokens.rbegin() + 1));
+						double g;
+						if(ssg >> g)
+						{
+							std::stringstream ssr(*(tokens.rbegin() + 2));
+							double r;
+							if(ssr >> r)
+							{
+								description.setrgbcolorFill(r, g, b);
+							}
+						}
+					}
+				}
+				tokens.clear();
+				break;
+			case Cmd::kRgbStroke:
+				if(tokens.size() >= 3)
+				{
+					std::stringstream ssb(*(tokens.rbegin()));
+					double b;
+					if(ssb >> b)
+					{
+						std::stringstream ssg(*(tokens.rbegin() + 1));
+						double g;
+						if(ssg >> g)
+						{
+							std::stringstream ssr(*(tokens.rbegin() + 2));
+							double r;
+							if(ssr >> r)
+							{
+								description.setrgbcolorStroke(r, g, b);
+							}
+						}
+					}
+				}
+				tokens.clear();
+				break;
+			case Cmd::kDash:
+				if(tokens.size() >= 1)
+				{
+					std::stringstream ssp(*(tokens.rbegin()));
+					pdf1_0::Integer p;
+					if(ssp >> p)
+					{
+						tokens.pop_back();
+						std::vector<pdf1_0::Integer> array;
+						for(auto i : tokens)
+						{
+							std::stringstream ssa(i);
+							pdf1_0::Integer a;
+							if(ssa >> a)
+							{
+								array.push_back(a);
+							}
+						}
+						description.setdash(p, array);
+					}
+				}
+				tokens.clear();
+				break;
+			case Cmd::kGrayFill:
+				if(tokens.size() >= 1)
+				{
+					std::stringstream ssgray(*(tokens.rbegin()));
+					double gray;
+					if(ssgray >> gray)
+					{
+						description.setgrayFill(gray);
+					}
+				}
+				tokens.clear();
+				break;
+			default:
+				tokens.push_back(token);
 			}
-			tokens.clear();
-			break;
-		default:
-			tokens.push_back(token);
 		}
 	}
 	if(!istream.fail())

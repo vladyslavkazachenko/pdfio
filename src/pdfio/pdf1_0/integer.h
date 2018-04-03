e9b380b0bd7c64bd385f12b786abf366b400a3dc
Vladyslav Kazachenko <vladyslav.kazachenko@tomtom.com>
2018-04-03 11:38:01 +0200

    pdf1_0: add Numeric<T>; make Integer a typedef Numeric<int>

--- src/pdfio/pdf1_0/integer.h
+++ src/pdfio/pdf1_0/integer.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "numeric.h"
+
 namespace pdfio
 {
 	
@@ -7,42 +9,7 @@ namespace pdf1_0
 {
 	
 /*! \brief PDF integer object.*/
-class Integer
-{
-public:
-	/*! \brief Default constructor.*/
-	Integer() = default;
-	/*! \brief Initializes integer by long long value.*/
-	inline explicit Integer(long long value)
-	: value_(value)
-	{
-		
-	}
-	/*! \brief Compares integer with long long value.*/
-	inline bool operator==(long long value) const
-	{
-		return value_ == value;
-	}
-	/*! \brief Compares integer with Integer value.*/
-	inline bool operator==(const Integer &other) const
-	{
-		return value_ == other.value_;
-	}
-	/*! \brief Assigns long long value to integer.*/
-	inline Integer &operator=(long long value)
-	{
-		value_ = value;
-		return *this;
-	}
-	/*! \brief Returns internal value.*/
-	inline operator long long() const
-	{
-		return value_;
-	}
-		
-private:
-	long long value_ = 0;
-};
+typedef Numeric<int> Integer;
 
 }
 

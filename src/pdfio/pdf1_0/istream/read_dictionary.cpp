19d5c36a37c96ae10e0bf49d80d3daf69a5f1332
Vladyslav Kazachenko <vladyslav.kazachenko@tomtom.com>
2018-04-04 15:45:03 +0200

    pdf1_0: add debug logs to istream>>Dictionary

--- src/pdfio/pdf1_0/istream/read_dictionary.cpp
+++ src/pdfio/pdf1_0/istream/read_dictionary.cpp
@@ -1,7 +1,5 @@
 #include "read_dictionary.h"
 
-#include <iostream>
-
 #include "read_name.h"
 #include "pdfio/pdf1_0/generic_object_type.h"
 #include "read_generic_object.h"
@@ -11,85 +9,94 @@ namespace pdf1_0 = pdfio::pdf1_0;
 
 std::istream &operator>>(std::istream &istream, pdf1_0::Dictionary &dictionary)
 {
-  LOG_DEBUG(__PRETTY_FUNCTION__ << ":dictionary=" << std::hex << std::showbase << 
-    reinterpret_cast<unsigned long>(&dictionary) << "\n");
-	std::string buffer;
-	while(istream && std::isspace(istream.peek()))
-	{
-		static_cast<void>(istream.get());
-	}
-	if(istream)
-	{
-		char tmp[3] = {0, 0, 0};
-		if(istream.read(tmp, 2))
-		{
-			buffer = tmp;
-		}
-	}
-	if(istream)
-	{
-		if(buffer == "<<")
-		{
-			auto streamPosition = istream.tellg();
-			std::set<pdf1_0::Name> keys = dictionary.keys();
-			while(istream)
-			{
-				streamPosition = istream.tellg();
-				pdf1_0::Name name;
-				if(istream >> name)
-				{
-					if(dictionary.contains(name))
-					{
-						if(istream >> dictionary.get(name))
-						{
-							keys.erase(name);
-							streamPosition = istream.tellg();
-						}
-						else
-						{
-							std::cerr << "failed to read " << std::string(name) << "\n";
-						}
-					}
-					else
-					{
-						std::cerr << __PRETTY_FUNCTION__ << ":unknown name " << std::string(name) << "\n" ;
-						istream.setstate(std::ios_base::failbit);
-					}
-				}
-				else
-				{
-					std::cerr << __PRETTY_FUNCTION__ << ":failed to read key\n";
-				}
-			}
-			istream.clear();
-			if(istream.seekg(streamPosition))
-			{
-				if(istream >> buffer)
-				{
-					if(buffer != ">>")
-					{
-						std::cerr << __PRETTY_FUNCTION__ << ":failed to read second delimiter\n";
-						istream.setstate(std::ios_base::failbit);
-					}
-					else
-					{
-						for(auto key : keys)
-						{
-							dictionary.remove(key);
-						}
-					}
-				}
-			}
-		}
-		else
-		{
-			std::cerr << __PRETTY_FUNCTION__ << ":invalid first delimiter\n";
-			istream.setstate(std::ios_base::failbit);
-		}
-	}
-	else
-	{
-		std::cerr << __PRETTY_FUNCTION__ << ":failed to read first delimiter\n";
-	}
-	return istream;
+  LOG_DEBUG(__PRETTY_FUNCTION__ << ":dictionary[" << std::hex << std::showbase << 
+    reinterpret_cast<unsigned long>(&dictionary) << "]:enter\n");
+  std::string buffer;
+  while(istream && std::isspace(istream.peek()))
+  {
+    LOG_DEBUG(__PRETTY_FUNCTION__ << ":dictionary[" << std::hex << std::showbase << 
+    reinterpret_cast<unsigned long>(&dictionary) << "]:skipping space character:" << std::hex << std::showbase << 
+    reinterpret_cast<int>(istream.get()) << "\n");
+  }
+  if(istream)
+  {
+    char tmp[3] = {0, 0, 0};
+    if(istream.read(tmp, 2))
+    {
+      buffer = tmp;
+      LOG_DEBUG(__PRETTY_FUNCTION__ << ":dictionary[" << std::hex << std::showbase << 
+        reinterpret_cast<unsigned long>(&dictionary) << "]:1st delimiter is " << buffer << "\n");
+    }
+  }
+  if(istream)
+  {
+    if(buffer == "<<")
+    {
+      auto streamPosition = istream.tellg();
+      std::set<pdf1_0::Name> keys = dictionary.keys();
+      while(istream)
+      {
+        streamPosition = istream.tellg();
+        pdf1_0::Name name;
+        if(istream >> name)
+        {
+          LOG_DEBUG(__PRETTY_FUNCTION__ << ":dictionary[" << std::hex << std::showbase << 
+            reinterpret_cast<unsigned long>(&dictionary) << "]:key is " << std::string(name) << "\n");
+          if(dictionary.contains(name))
+          {
+            if(istream >> dictionary.get(name))
+            {
+              keys.erase(name);
+              streamPosition = istream.tellg();
+            }
+            else
+            {
+              std::cerr << "failed to read " << std::string(name) << "\n";
+            }
+          }
+          else
+          {
+            std::cerr << __PRETTY_FUNCTION__ << ":unknown name " << std::string(name) << "\n" ;
+            istream.setstate(std::ios_base::failbit);
+          }
+        }
+        else
+        {
+          LOG_DEBUG(__PRETTY_FUNCTION__ << ":dictionary[" << std::hex << std::showbase << 
+            reinterpret_cast<unsigned long>(&dictionary) << "]:token is not a name\n");
+        }
+      }
+      istream.clear();
+      if(istream.seekg(streamPosition))
+      {
+        if(istream >> buffer)
+        {
+          if(buffer != ">>")
+          {
+            std::cerr << __PRETTY_FUNCTION__ << ":failed to read second delimiter\n";
+            istream.setstate(std::ios_base::failbit);
+          }
+          else
+          {
+            for(auto key : keys)
+            {
+              dictionary.remove(key);
+            }
+          }
+        }
+      }
+    }
+    else
+    {
+      std::cerr << __PRETTY_FUNCTION__ << ":invalid first delimiter\n";
+      istream.setstate(std::ios_base::failbit);
+    }
+  }
+  else
+  {
+    std::cerr << __PRETTY_FUNCTION__ << ":failed to read first delimiter\n";
+  }
+  LOG_DEBUG(__PRETTY_FUNCTION__ << ":dictionary[" << std::hex << std::showbase << 
+    reinterpret_cast<unsigned long>(&dictionary) << "]:leave\n");
+  return istream;
 }

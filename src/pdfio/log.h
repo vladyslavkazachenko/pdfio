f084075994c30810ce21ec11df82a92102251ae3
Vladyslav Kazachenko <vladyslav.kazachenko@tomtom.com>
2018-04-13 15:21:38 +0200

    pdfio: fix build when PDF_LOG is not defined

--- src/pdfio/log.h
+++ src/pdfio/log.h
@@ -1,23 +1,24 @@
 #pragma once
 
-#ifdef PDF_LOG
-
 #include <functional>
 #include <string>
-#include <sstream>
 
 namespace pdfio
 {
   
 extern std::function<void(const std::string &msg)> gLogErrorHandler;
 extern std::function<void(const std::string &msg)> gLogDebugHandler;
+  
+}
+
+#ifdef PDF_LOG
+
+#include <sstream>
 
 #define LOG(msg, logHandler) {std::stringstream logStream; logStream << msg;\
   if(logHandler){logHandler(logStream.str());}}
 #define LOG_DEBUG(msg) LOG(msg, pdfio::gLogDebugHandler)
 #define LOG_ERROR(msg) LOG(msg, pdfio::gLogErrorHandler)
-  
-}
 
 #else
 

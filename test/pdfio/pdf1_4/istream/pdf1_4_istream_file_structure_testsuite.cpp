2caf7757a0cb296ba64258d61e727ba78ec4973a
Vladyslav Kazachenko <vladyslav.kazachenko@tomtom.com>
2018-05-02 10:12:59 +0200

    pdf1_4: clean FileStructureTestSuite

--- test/pdfio/pdf1_4/istream/pdf1_4_istream_file_structure_testsuite.cpp
+++ test/pdfio/pdf1_4/istream/pdf1_4_istream_file_structure_testsuite.cpp
@@ -5,36 +5,6 @@
 
 namespace pdf1_4 = pdfio::pdf1_4;
 
-TEST(FileStructureTestSuite, trailer1)
-{
-  pdf1_4::FileStructure::Trailer trailer;
-  std::istringstream istream(
-    "trailer\n"
-    "<< /Size 22"
-    "/Root 2 0 R"
-    "/Info 1 0 R"
-    "/ID [ < 81b14aafa313db63dbd6f981e49f94f4 >"
-    "< 81b14aafa313db63dbd6f981e49f94f4 >"
-    "]"
-    ">>"
-    "startxref\n"
-    "18799\n"
-    "%%EOF"
-  );
-  EXPECT_TRUE(istream >> trailer);
-  EXPECT_EQ(22, trailer.size());
-  EXPECT_TRUE(trailer.hasRoot());
-  EXPECT_EQ(2, trailer.root().objectNumber());
-  EXPECT_EQ(0, trailer.root().generationNumber());
-  EXPECT_TRUE(trailer.hasInfo());
-  EXPECT_EQ(1, trailer.info().objectNumber());
-  EXPECT_EQ(0, trailer.info().generationNumber());
-  EXPECT_TRUE(trailer.hasId());
-  EXPECT_EQ(2, trailer.id().size());
-  EXPECT_TRUE(trailer.id()[0] == "81b14aafa313db63dbd6f981e49f94f4");
-  EXPECT_TRUE(trailer.id()[1] == "81b14aafa313db63dbd6f981e49f94f4");
-}
-
 TEST(FileStructureTestSuite, ok1)
 {
    pdfio::FileStructure<pdf1_4::FileVersion> fileStruct;
@@ -124,4 +94,9 @@ TEST(FileStructureTestSuite, ok1)
    EXPECT_EQ(384, subsection.entries_[6].offset_);
    EXPECT_EQ(0, subsection.entries_[6].generation_);
    EXPECT_TRUE(subsection.entries_[6].inUseFlag_);
+   const auto &trailer = fileStruct.versions_.front().trailer_;
+   EXPECT_EQ(7, trailer.size());
+   EXPECT_TRUE(trailer.hasRoot());
+   EXPECT_EQ(1, trailer.root().objectNumber());
+   EXPECT_EQ(0, trailer.root().generationNumber());
 }
